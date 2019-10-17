73195e136b7d5ad92a1662af2d01e1eeddca809f
Lovepreet Thind <lovepreetthind@Administrators-MacBook-Pro.local>
2019-10-17 11:38:40 -0400

    Testing Heroku

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   uri = process.env.ATLAS_URI;
 } else {  
   // localhost
-  uri = process.env.LOCAL_URI  
+  uri = process.env.ATLAS_URI
 }
 
 // database connection ppol
